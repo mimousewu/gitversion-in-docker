c15f38f890b208e5eb01fcb1bfc50de73abb0730
TW-Dev-WuTao <tawu@thoughtworks.com>
2019-01-23 10:24:13 +0800

    update folder

--- genGitversion.sh
+++ genGitversion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WORKSPACE=$1
 if [ ! -z $(docker images |grep -E "^gitversion" | wc -l) ]; then
-docker build -f gitversion/Dockerfile-gitversion -t gitversion giversion/
+docker build -f gitversion/Dockerfile-gitversion -t gitversion gitversion/
 fi
 docker run --rm -v jenkins_jenkins_home:/var/jenkins_home -e APP=${WORKSPACE} gitversion > .gitversion.json
